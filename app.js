26f29a05d7cc42799ae20f189530519823dc4f17
Alfonso Rafael Camacho Guerra <alfonsorafaelcamacho@gmail.com>
2024-03-12 14:24:26 -0600

    03 Proyecto inicial

---
+++ app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('h1');    //Objeto al cual se le coloca un texto
+let parrafo = document.querySelector('p');
+
+titulo.innerHTML = "Juego del numero secreto"; //Se le aplica un metodo al objeto titulo
+parrafo.innerHTML = "Indica un numero del 1 al 10"; //Se aplica un metodo al objeto parrafo
