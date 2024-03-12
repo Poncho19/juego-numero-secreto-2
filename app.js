98ddfd4312edf6eb22db948adbb3792089983a2f
Alfonso Rafael Camacho Guerra <alfonsorafaelcamacho@gmail.com>
2024-03-12 15:38:39 -0600

    04 genera un numero aleatorio

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
+let numeroSecreto = 0;
+let rango = 10;
+
 //Titulos
 asignarTextoElemento('h1', "Juego del número secreto");//Se le aplica un metodo al objeto titulo
-asignarTextoElemento('p', "Indica un numero del 1 al 10");//Se aplica un metodo al objeto parrafo
+asignarTextoElemento('p', `Indica un numero del 1 al ${rango}`);//Se aplica un metodo al objeto parrafo
 
+//Se genera el numero secreto del juego
+numeroSecreto = generarNumeroSecreto(rango);
+console.log(numeroSecreto);
 
 
 
 //Funcion botones
 function asignarTextoElemento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);    //Objeto al cual se le coloca un texto
-    elementoHTML.innerHTML = texto;                         //Se aplica un texto al elemento HTML
+    elementoHTML.innerHTML = texto;
+    return;                         //Se aplica un texto al elemento HTML
 }
 
-function intentoDeUsuario(){
-    alert("Click desde el boton");
+function generarNumeroSecreto(rango){
+    let numeroAleatorio = parseInt(Math.random()*rango) + 1;
+    return numeroAleatorio;
 }
