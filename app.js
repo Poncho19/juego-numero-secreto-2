8b26ee0555148f1f57bfe63cdd5a34a13d5aef8b
Alfonso Rafael Camacho Guerra <alfonsorafaelcamacho@gmail.com>
2024-03-12 15:26:31 -0600

    02 asigna texto a un elemento HTML

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-let titulo = document.querySelector('h1');    //Objeto al cual se le coloca un texto
-let parrafo = document.querySelector('p');
-
 //Titulos
-titulo.innerHTML = "Juego del numero secreto"; //Se le aplica un metodo al objeto titulo
-parrafo.innerHTML = "Indica un numero del 1 al 10"; //Se aplica un metodo al objeto parrafo
+asignarTextoElemento('h1', "Juego del número secreto");//Se le aplica un metodo al objeto titulo
+asignarTextoElemento('p', "Indica un numero del 1 al 10");//Se aplica un metodo al objeto parrafo
+
+
+
 
 //Funcion botones
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);    //Objeto al cual se le coloca un texto
+    elementoHTML.innerHTML = texto;                         //Se aplica un texto al elemento HTML
+}
+
 function intentoDeUsuario(){
     alert("Click desde el boton");
 }
